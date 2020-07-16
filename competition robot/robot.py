aba372a8d857927af93c9a85cb32bc6768c552cb
F-gigabyte <57836145+F-gigabyte@users.noreply.github.com>
2020-07-16 22:28:41 +0100

    Update robot.py
    Extended it for more than 2 cubes in a very bad, will probably fail way.

--- competition robot/robot.py
+++ competition robot/robot.py
@@ -6,7 +6,7 @@ main_cubes = [[47, 45], [45, 46], [46, 44], [44, 47]]
 secondary_cubes = [[44, 46], [47, 44], [45, 47], [46, 45]] # for extending cube range
 #return_markers = [27, 6, 13, 21] initially so it didn't push first cube out of zone when going for second but now can't see start zone so drops in opponent zone
 return_markers = [[27, 0], [6, 7], [13, 14], [20, 21]]
-mid_term_markers = [[[24, 25], [2, 3]], [[3, 4], [9, 10]], [[10, 11], [16, 17]], [[17, 18], [23, 24]]] # for extending cube range
+mid_term_markers = [[[23], [4]], [[2], [11]], [[9], [18]], [[16], [25]]] # for extending cube range
 seek_markers = [20, 27, 6, 13]
 
 """Get to cubes"""
@@ -24,17 +24,38 @@ R.openGripper()
 target = main_cubes[R.zone][iteration]
 return_marker = None
 R.seek_marker(target, 10, -10)
-while True and iteration < len(main_cubes[R.zone]):
-    target = main_cubes[R.zone][iteration]
+num_primary = len(main_cubes[R.zone])
+while True and iteration < num_primary + len(secondary_cubes[R.zone]):
+    print(iteration)
+    if iteration < num_primary:
+        target = main_cubes[R.zone][iteration]
+    else:
+        target = secondary_cubes[R.zone][iteration - num_primary]
     while not R.have_cube:
-        R.seek_marker(target)
-        R.drive_to_marker(target, 0.4, 80)
-        R.turn_to_marker(target, 0.5, True)
+        if iteration < num_primary:
+            R.seek_marker(target)
+            R.drive_to_marker(target, 0.4, 80)
+            R.turn_to_marker(target, 0.2, True)
+        else:
+            R.seek_markers_with_priority([target], mid_term_markers[R.zone][iteration - num_primary])
+            R.drive_to_markers_with_priority([target], mid_term_markers[R.zone][iteration - num_primary], 1.5, 80)
+            R.seek_marker(target)
+            R.drive_to_marker(target, 0.4, 80)
+            R.turn_to_marker(target, 0.2, True)
         R.setDriveMotors(50)
         R.sleep(0.4)
         R.stopDriveMotors()
         R.closeGripper()
-    if iteration < len(main_cubes[R.zone]) - 1:
+    if iteration < num_primary:
+        R.seek_markers(return_markers[R.zone], 10, 10)
+        R.drive_to_markers(return_markers[R.zone], 1.3, 80, 0.2, 3, 20)
+        R.openGripper()
+        R.setDriveMotors(-60)
+        R.sleep(0.2)
+        R.stopDriveMotors()
+    elif iteration < num_primary + len(secondary_cubes) -1:
+        R.seek_markers_with_priority(return_markers[R.zone], mid_term_markers[R.zone][iteration - num_primary], 10, 10)
+        R.drive_to_markers_with_priority(return_markers[R.zone], mid_term_markers[R.zone][iteration - num_primary], 1.5, 80, 0.2, 3, 20)
         R.seek_markers(return_markers[R.zone], 10, 10)
         R.drive_to_markers(return_markers[R.zone], 1.3, 80, 0.2, 3, 20)
         R.openGripper()
@@ -42,8 +63,10 @@ while True and iteration < len(main_cubes[R.zone]):
         R.sleep(0.2)
         R.stopDriveMotors()
     else:
+        R.seek_markers_with_priority(return_markers[R.zone], mid_term_markers[R.zone][iteration - num_primary], 10, 10)
+        R.drive_to_markers_with_priority(return_markers[R.zone], mid_term_markers[R.zone][iteration - num_primary], 1.5, 80, 0.2, 3, 20)
         R.seek_markers(return_markers[R.zone], 10, 10)
-        R.drive_to_markers(return_markers[R.zone], 1.3, 80, 0.2, 3, 100)
+        R.drive_to_markers(return_markers[R.zone], 1.3, 80, 0.2, 3, 20)
         R.openGripper()
         R.sleep(1)
     iteration += 1
